1d53cfcdb47cae6f6efeb616c22bdc9e6c7acb1c
naodroid <naodroid@gmail.com>
2021-12-12 05:54:42 +0900

    Add QuantizerCelebi

--- MateriatColorSwift/quantize/QuantizerCelebi.swift
+++ MateriatColorSwift/quantize/QuantizerCelebi.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+
+class QuantizerCelebi: Quantizer {
+    func quantize(pixels: [Int], maxColors: Int) async -> QuantizerResult {
+        let wu = QuantizerWu()
+        let wuResult = await wu.quantize(pixels: pixels, maxColors: maxColors)
+        let clusters: [Int] = Array(wuResult.colorToCount.keys)
+        let wsmeansResult = QuantizerWsmeans.quantize(
+            inputPixels: pixels,
+            maxColors: maxColors,
+            startingClusters: clusters,
+            pointProvider: PointProviderLab())
+        return wsmeansResult;
+    }
+}
