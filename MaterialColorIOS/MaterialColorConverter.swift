9326096b75d5783c3f17f6536bc7286d59a4925d
naodroid <naodroid@gmail.com>
2021-12-14 08:13:41 +0900

    Fix color pick issues

--- MaterialColorIOS/MaterialColorConverter.swift
+++ MaterialColorIOS/MaterialColorConverter.swift
@@ -11,6 +11,16 @@ import CoreGraphics
 import CoreImage
 import MaterialColorSwift
 
+private struct ColorCount: Comparable {
+    let color: Int
+    let count: Int
+    static func ==(lhs: ColorCount, rhs: ColorCount) -> Bool {
+        return lhs.count == rhs.count
+    }
+    static func <(lhs: ColorCount, rhs: ColorCount) -> Bool {
+        return lhs.count < rhs.count
+    }
+}
 
 public class MaterialColorConverter {
     
@@ -23,23 +33,32 @@ public class MaterialColorConverter {
             inputPixels: pixels,
             maxColors: size
         ).colorToCount
-        let colors = Array(colorToCount.keys)
-        return isDarkTheme ? Scheme.dark(color: colors[0]) : Scheme.light(color: colors[0])
+        let colors: [ColorCount] = colorToCount.map { (k, v) in
+            ColorCount(color: k, count: v)
+        }.sorted().reversed()
+        
+        //show histogram for debug
+//        for c in colors {
+//            print(String(format: "%02X, %d", c.color, c.count))
+//        }
+        //pick top used color
+        let color = colors[0].color
+        return isDarkTheme ? Scheme.dark(color: color) : Scheme.light(color: color)
     }
+    
+    
     /// convert uiimage to argb pixel array in specified size (width, height=size)
-    public static func imageToPixels(_ uiImage: UIImage, size: Int = 64) -> [Int]? {
+    public static func imageToPixels(_ uiImage: UIImage, size: Int = 16) -> [Int]? {
         guard let cgImage = uiImage.cgImage else {
             return nil
         }
-        let inputWidth = cgImage.width
-        let inputHeight = cgImage.height
         let outputWidth = size
         let outputHeight = size
         let bytesPerPixel = 4
-        let bytesPerRow = bytesPerPixel * inputWidth
+        let bytesPerRow = bytesPerPixel * outputWidth
         let bitsPerComponent = 8
-        
-        var pixels: [UInt32] = Array(repeating: 1, count: inputWidth * inputHeight)
+        let bitmapInfo = CGImageAlphaInfo.premultipliedFirst.rawValue
+        var pixels: [UInt32] = Array(repeating: 0, count: outputWidth * outputHeight)
         let colorSpace = CGColorSpaceCreateDeviceRGB()
         
         let contextOpt = pixels.withUnsafeMutableBytes { ptr -> CGContext? in
@@ -47,21 +66,25 @@ public class MaterialColorConverter {
                 return nil
             }
             return CGContext(data: raw,
-                             width: inputWidth,
-                             height: inputHeight,
+                             width: outputWidth,
+                             height: outputHeight,
                              bitsPerComponent: bitsPerComponent,
                              bytesPerRow: bytesPerRow,
                              space: colorSpace,
-                             bitmapInfo: CGImageAlphaInfo.premultipliedFirst.rawValue)
+                             bitmapInfo: bitmapInfo)
         }
         guard let context = contextOpt else {
             return nil
         }
         // resize for reducing memory
         context.draw(cgImage, in: CGRect(x: 0, y: 0, width: outputWidth, height: outputHeight))
-        // int32 to int
+        // int32-bgra to int-argb
         let result: [Int] = pixels.map {
-            return Int($0)
+            let b = Int(($0 >> 24) & 0xFF)
+            let g = Int(($0 >> 16) & 0xFF)
+            let r = Int(($0 >> 8) & 0xFF)
+            let a = Int($0 & 0xFF)
+            return (a << 24) | (r << 16) | (g << 8) | b
         }
         return result
     }
